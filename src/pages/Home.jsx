c1c92371c55264d67cc9ecefb876311e818ca6d5
muhammadwasikt <wasikhatri11@gmail.com>
2024-10-25 14:49:26 +0500

    solve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Banner from "../component/common/Banner";
 import FeatureCategory from "../component/common/FeatureCategory";
 import FeaturedProducts from "../component/common/FeaturedProducts";
 import FleshSale from "../component/common/FleshSale";
-import MainCard from "../component/common/mainCard";
+import MainCard from "../component/common/MainCard";
 import NewArrivals from "../component/common/NewArrivals";
 import Header from "../component/Header"
 
