c6ec0e51c9fcb39c8544ecd94afec43fad3375f1
muhammadwasikt <wasikhatri11@gmail.com>
2024-10-25 14:44:11 +0500

    error

--- src/component/common/MainCard.jsx
+++ src/component/common/MainCard.jsx
@@ -1,11 +1,10 @@
-
 const MainCard = () => {
-  return(
-    <div className="w-[100%] flex justify-between items-center py-4 max-md:flex-col gap-3 md:pr-4">
-    <img src='https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fgrid%2Fbanner-4.png&w=1080&q=100' className='rounded w-[50%] h-[280px] max-md:w-[100%]' />
-    <img src='https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fgrid%2Fbanner-5.png&w=1920&q=100' className='rounded w-[50%] h-[280px] max-md:w-[100%]' />
-</div>
-  )
-}
-
-export default MainCard
+    return(
+      <div className="w-[100%] flex justify-between items-center py-4 max-md:flex-col gap-3 md:pr-4">
+      <img src='https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fgrid%2Fbanner-4.png&w=1080&q=100' className='rounded w-[50%] h-[280px] max-md:w-[100%]' />
+      <img src='https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fbanner%2Fgrid%2Fbanner-5.png&w=1920&q=100' className='rounded w-[50%] h-[280px] max-md:w-[100%]' />
+  </div>
+    )
+  }
+  
+  export default MainCard
